ca119cf106ff6450506bd6138ecc776879807c64
xujunyu <1098672878@qq.com>
2018-05-07 10:49:18 +0800

    相同的biztype下，报告进行融合

--- src/main/java/top/palexu/blockchaincredit/report/facade/impl/SearchReportServiceImpl.java
+++ src/main/java/top/palexu/blockchaincredit/report/facade/impl/SearchReportServiceImpl.java
@@ -15,9 +15,7 @@ import top.palexu.blockchaincredit.report.facade.SearchReportJson;
 import top.palexu.blockchaincredit.report.facade.SearchReportService;
 import top.palexu.blockchaincredit.report.service.ReportService;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @Slf4j
@@ -39,35 +37,42 @@ public class SearchReportServiceImpl implements SearchReportService {
         //用subject找到所有相关数据
         List<CreditDataRecord> recordList = creditDataStoreService.selectAllRecordBySubject(subject);
 
+        Map<String, List<CreditDataRecord>> recordMap = sortByBizType(recordList);
+
         //按照bizType不同，选择不同报告生成器进行计算
         //不同bizType的数据和因子不能做融合
-        for (CreditDataRecord record : recordList) {
-            String bizType = record.getBizType();
-            ReportContext context = new ReportContext(subject, record.getProvider(), bizType);
+        for (String bizType : recordMap.keySet()) {
             Report report = new Report();
 
-            Map<String, Factor> factorMap = null;
-            //todo 改为在reportservice中处理name 和 desc
-            if (BizTypeEnum.creditCard.getValue().equals(bizType)) {
-                factorMap = reportService.singleReport(context);
-                report.setReportName("信用卡报告");
-                report.setDesc("信用卡报告-DATAHUB");
-            } else if (BizTypeEnum.sharedBike.getValue().equals(bizType)) {
-                factorMap = reportService.singleReport(context);
-                report.setReportName("共享单车报告");
-                report.setDesc("测试");
-            } else {
-                log.error("查询报告,没有找到对应的报告处理器,bizType={}", record.getBizType());
-            }
+            //组合所有的因子结果
+            for (CreditDataRecord record : recordMap.get(bizType)) {
+                ReportContext context = new ReportContext(subject, record.getProvider(), bizType);
 
-            //如果没有结果则跳过
-            if (null == factorMap) {
-                log.error("查询报告,计算报告结果为空,bizType={}", record.getBizType());
-                continue;
+                Map<String, Factor> factorMap = null;
+                //todo 改为在reportservice中处理name 和 desc
+                if (BizTypeEnum.creditCard.getValue().equals(bizType)) {
+                    factorMap = reportService.singleReport(context);
+                    report.setReportName("信用卡报告");
+                    report.setDesc("信用卡报告-DATAHUB");
+                } else if (BizTypeEnum.sharedBike.getValue().equals(bizType)) {
+                    factorMap = reportService.singleReport(context);
+                    report.setReportName("共享单车报告");
+                    report.setDesc("测试");
+                } else {
+                    log.error("查询报告,没有找到对应的报告处理器,bizType={}", record.getBizType());
+                }
+
+                //如果没有结果则跳过
+                if (null == factorMap) {
+                    log.error("查询报告,计算报告结果为空,bizType={}", record.getBizType());
+                    continue;
+                }
+
+                report.getFactors().addAll(factorMap.values());
             }
 
             //设置通用属性
-            report.getFactors().addAll(factorMap.values());
+
             report.setBizType(bizType);
             report.setSubject(subject);
             report.setGmtCreated(new Date());
@@ -80,5 +85,22 @@ public class SearchReportServiceImpl implements SearchReportService {
         JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd";
         return JSON.toJSONString(searchReportJson, SerializerFeature.WriteDateUseDateFormat,
                                  SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    private Map<String, List<CreditDataRecord>> sortByBizType(List<CreditDataRecord> recordList) {
+        if (null == recordList || recordList.size() == 0) {
+            return Collections.emptyMap();
+        }
+
+        Map<String, List<CreditDataRecord>> result = new HashMap<>();
+        for (CreditDataRecord record : recordList) {
+            if (!result.containsKey(record.getBizType())) {
+                List<CreditDataRecord> list = new ArrayList<>();
+                result.put(record.getBizType(), list);
+            }
+            result.get(record.getBizType()).add(record);
+        }
+
+        return result;
     }
 }
